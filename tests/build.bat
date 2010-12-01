567dd6ef33a484c7a4cef49031eb118be87f1b59
Matthijs Galesloot <matthijs.galesloot@gmail.com>
2010-12-01 19:34:13 +0000

    Web plugin - final pom fixes

--- tests/build.bat
+++ tests/build.bat
@@ -1,16 +1,16 @@
 
 rem run this script from web folder
 
+set M2_HOME=C:\bin\apache-maven-3.0
 set SONAR_HOME=C:\bin\sonar-2.4.1
-set SONAR_FLAGS=-Dsonar.language=web 
-rem -Dsonar.dynamicAnalysis=false -Dsonar.jdbc.url=jdbc:postgresql://localhost/sonar -Dsonar.jdbc.driver=org.postgresql.Driver -Dsonar.jdbc.username=sonar -Dsonar.jdbc.password=sonar
+set SONAR_FLAGS=-Dsonar.language=web -Dsonar.dynamicAnalysis=false -Dsonar.jdbc.url=jdbc:postgresql://localhost/sonar -Dsonar.jdbc.driver=org.postgresql.Driver -Dsonar.jdbc.username=sonar -Dsonar.jdbc.password=sonar
 set DEBUG=-X 
 
-call mvn install -Dmaven.test.skip
+call %M2_HOME%\bin\mvn install -Dmaven.test.skip
 call xcopy /Y target\sonar-web-plugin-1.0-SNAPSHOT.jar %SONAR_HOME%\extensions\plugins
 start "Sonar Server" /MIN %SONAR_HOME%\bin\windows-x86-32\StartSonar.bat 
 
-set mvncommand=mvn sonar:sonar
+set mvncommand=%M2_HOME%\bin\mvn sonar:sonar
 
 :mvn
 rem 'ping' in order to wait a few seconds
@@ -18,7 +18,7 @@ ping 127.0.0.1 -n 10 -w 1000 > nul
 rem try mvn sonar
 call %mvncommand% -f d:\workspaces\sonar\testweb\alfresco-web-client\pom.xml %SONAR_FLAGS% %DEBUG% > sonar-alfresco.log
 rem check if sonar was available
-find "[INFO] Sonar server can not be reached" *.log
+find "Sonar server can not be reached" *.log
 rem previous command will set errorlevel to 0 if the log contained "sonar can not be reached"
 IF %ERRORLEVEL% == 0 GOTO mvn
 
