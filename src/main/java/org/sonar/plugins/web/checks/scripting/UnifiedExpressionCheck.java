e5da1808abc03cc1992c69a466d083865c1fb317
Fabrice Bellingard <bellingard@gmail.com>
2012-06-04 07:57:48 +0000

    Web plugin - Add temporary AL2 header template inside the plugin to remove the trailing semi-colon that produces false positive in Sonar

--- src/main/java/org/sonar/plugins/web/checks/scripting/UnifiedExpressionCheck.java
+++ src/main/java/org/sonar/plugins/web/checks/scripting/UnifiedExpressionCheck.java
@@ -3,7 +3,7 @@
  * Copyright (C) 2010 Matthijs Galesloot
  * dev@sonar.codehaus.org
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License")
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -15,7 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.sonar.plugins.web.checks.scripting;
 
 import org.apache.commons.lang.ArrayUtils;
