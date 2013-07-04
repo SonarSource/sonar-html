a9a30a5df20acd3e8ab2bb75493866832c5b2294
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2013-07-04 17:41:14 +0200

    Fix a quality flaw

---
+++ src/test/java/org/sonar/plugins/web/checks/dependencies/DynamicJspIncludeCheckTest.java
@@ -0,0 +1,41 @@
+/*
+ * Sonar Web Plugin
+ * Copyright (C) 2010 SonarSource and Matthijs Galesloot
+ * dev@sonar.codehaus.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sonar.plugins.web.checks.dependencies;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.sonar.plugins.web.checks.CheckMessagesVerifierRule;
+import org.sonar.plugins.web.checks.TestHelper;
+import org.sonar.plugins.web.visitor.WebSourceCode;
+
+import java.io.File;
+
+public class DynamicJspIncludeCheckTest {
+
+  @Rule
+  public CheckMessagesVerifierRule checkMessagesVerifier = new CheckMessagesVerifierRule();
+
+  @Test
+  public void detected() {
+    WebSourceCode sourceCode = TestHelper.scan(new File("src/test/resources/checks/DynamicJspIncludeCheck.jsp"), new DynamicJspIncludeCheck());
+
+    checkMessagesVerifier.verify(sourceCode.getViolations())
+        .next().atLine(2).withMessage("Dynamic includes are not allowed.");
+  }
+
+}
