e329a43185caaa5fca59d59fb1fe25af2856aa03
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2013-11-08 10:09:02 +0100

    SONARPLUGINS-3132 Rename the "Sonar" Web rule engine into "SonarQube"

--- src/main/java/org/sonar/plugins/web/rules/WebRulesRepository.java
+++ src/main/java/org/sonar/plugins/web/rules/WebRulesRepository.java
@@ -26,7 +26,7 @@ import java.util.List;
 
 public final class WebRulesRepository extends RuleRepository {
 
-  public static final String REPOSITORY_NAME = "Web";
+  public static final String REPOSITORY_NAME = "SonarQube";
   public static final String REPOSITORY_KEY = "Web";
   private final AnnotationRuleParser annotationRuleParser;
 
