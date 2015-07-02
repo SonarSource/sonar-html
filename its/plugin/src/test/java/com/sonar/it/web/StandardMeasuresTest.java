701273c03928f0ea227bc1477dcb72086f8507d8
Massimo Paladin <massimo.paladin@gmail.com>
2015-07-02 14:58:22 +0200

    Revert "Update ITs parent pom"
    This reverts commit 4175ac868183c6d577942278f429e374d0458065.

--- its/plugin/src/test/java/com/sonar/it/web/StandardMeasuresTest.java
+++ its/plugin/src/test/java/com/sonar/it/web/StandardMeasuresTest.java
@@ -1,7 +1,7 @@
 /*
- * SonarQube :: Web :: ITs :: Plugin
+ * SonarSource :: Web :: ITs :: Plugin
  * Copyright (C) 2011 SonarSource
- * sonarqube@googlegroups.com
+ * dev@sonar.codehaus.org
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
