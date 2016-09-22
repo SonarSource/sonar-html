aab2939f43e01ff577cce3f9ac5c1afab674bc3f
Massimo Paladin <massimo.paladin@gmail.com>
2016-09-22 11:33:39 +0200

    Upgrade to latest travis-utils

--- travis.sh
+++ travis.sh
@@ -4,21 +4,10 @@ set -euo pipefail
 
 function installTravisTools {
   mkdir ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v28 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v31 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
-
 installTravisTools
+. installJDK8
 
-case "$TEST" in
-
-ci)
-  regular_mvn_build_deploy_analyze
-  ;;
-
-*)
-  echo "Unexpected TEST mode: $TEST"
-  exit 1
-  ;;
-
-esac
+regular_mvn_build_deploy_analyze -Dmaven.test.redirectTestOutputToFile=true
