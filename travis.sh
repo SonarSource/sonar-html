eb796d6fed839a458ee2609b38441ff738be0057
Alban Auzeill <alban.auzeill@sonarsource.com>
2018-01-03 10:27:25 +0100

    Update travis-utils to v43

--- travis.sh
+++ travis.sh
@@ -4,11 +4,12 @@ set -euo pipefail
 
 function installTravisTools {
   mkdir -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v38 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v43 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 installTravisTools
 . installJDK8
+source ~/.local/bin/installMaven35
 
 export DEPLOY_PULL_REQUEST=true
 regular_mvn_build_deploy_analyze -Dmaven.test.redirectTestOutputToFile=true
