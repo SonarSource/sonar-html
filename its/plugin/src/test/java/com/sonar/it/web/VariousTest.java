987e988ea95eb0e13edc45c3c281719b666ac09d
Massimo Paladin <massimo.paladin@gmail.com>
2018-01-09 08:45:56 +0100

    ITs license:format

--- its/plugin/src/test/java/com/sonar/it/web/VariousTest.java
+++ its/plugin/src/test/java/com/sonar/it/web/VariousTest.java
@@ -1,6 +1,6 @@
 /*
  * SonarSource :: Web :: ITs :: Plugin
- * Copyright (c) 2011-2017 SonarSource SA and Matthijs Galesloot
+ * Copyright (c) 2011-2018 SonarSource SA and Matthijs Galesloot
  * sonarqube@googlegroups.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
