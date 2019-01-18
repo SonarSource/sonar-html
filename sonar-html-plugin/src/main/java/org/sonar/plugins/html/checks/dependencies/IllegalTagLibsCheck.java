41179281a34af01bb5269c00c33a7b5ed95ee7d2
Nicolas PERU <nicolas.peru@sonarsource.com>
2019-01-18 14:40:27 +0100

    Update license headers

--- sonar-html-plugin/src/main/java/org/sonar/plugins/html/checks/dependencies/IllegalTagLibsCheck.java
+++ sonar-html-plugin/src/main/java/org/sonar/plugins/html/checks/dependencies/IllegalTagLibsCheck.java
@@ -1,6 +1,6 @@
 /*
  * SonarHTML :: SonarQube Plugin
- * Copyright (c) 2010-2018 SonarSource SA and Matthijs Galesloot
+ * Copyright (c) 2010-2019 SonarSource SA and Matthijs Galesloot
  * sonarqube@googlegroups.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
