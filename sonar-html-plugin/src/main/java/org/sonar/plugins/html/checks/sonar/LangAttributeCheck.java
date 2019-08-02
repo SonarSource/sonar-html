dc0b7fbf399a95565175982c24119e3bcb147434
Yassin Kammoun <yassin.kammoun@sonarsource.com>
2019-08-02 10:19:08 +0200

    SONARHTML-97 Rule S5254: "<html>" element should have a language attribute

---
+++ sonar-html-plugin/src/main/java/org/sonar/plugins/html/checks/sonar/LangAttributeCheck.java
@@ -0,0 +1,41 @@
+/*
+ * SonarHTML :: SonarQube Plugin
+ * Copyright (c) 2010-2019 SonarSource SA and Matthijs Galesloot
+ * sonarqube@googlegroups.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sonar.plugins.html.checks.sonar;
+
+import org.sonar.check.Rule;
+import org.sonar.plugins.html.checks.AbstractPageCheck;
+import org.sonar.plugins.html.node.TagNode;
+
+@Rule(key = "S5254")
+public class LangAttributeCheck extends AbstractPageCheck {
+
+  @Override
+  public void startElement(TagNode node) {
+    if (isHtmlTag(node) && !hasLangAttribute(node)) {
+      createViolation(node.getStartLinePosition(), "Add \"lang\" and/or \"xml:lang\" attributes to this \"<html>\" element");
+    }
+  }
+
+  private static boolean isHtmlTag(TagNode node) {
+    return "HTML".equalsIgnoreCase(node.getLocalName());
+  }
+
+  private static boolean hasLangAttribute(TagNode node) {
+    return node.getAttribute("lang") != null || node.getAttribute("xml:lang") != null;
+  }
+}
