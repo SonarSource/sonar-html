b8bfe86b35da7e3ccf61d1a03afabd9033da00f5
Yassin Kammoun <yassin.kammoun@sonarsource.com>
2019-08-02 10:50:57 +0200

    SONARHTML-100 Rule S5257: HTML "<table>" should not be used for layout purposes

---
+++ sonar-html-plugin/src/main/java/org/sonar/plugins/html/checks/sonar/LayoutTableCheck.java
@@ -0,0 +1,42 @@
+/*
+ * SonarHTML :: SonarQube Plugin
+ * Copyright (c) 2010-2019 SonarSource SA and Matthijs Galesloot
+ * sonarqube@googlegroups.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sonar.plugins.html.checks.sonar;
+
+import org.sonar.check.Rule;
+import org.sonar.plugins.html.checks.AbstractPageCheck;
+import org.sonar.plugins.html.node.TagNode;
+
+@Rule(key = "S5257")
+public class LayoutTableCheck extends AbstractPageCheck {
+
+  @Override
+  public void startElement(TagNode node) {
+    if (isTable(node) && isLayout(node)) {
+      createViolation(node.getStartLinePosition(), "Replace this layout table with a CSS layout.");
+    }
+  }
+
+  private static boolean isTable(TagNode node) {
+    return "TABLE".equalsIgnoreCase(node.getLocalName());
+  }
+
+  private static boolean isLayout(TagNode node) {
+    String role = node.getAttribute("role");
+    return role != null && ("PRESENTATION".equalsIgnoreCase(role) || "NONE".equalsIgnoreCase(role));
+  }
+}
