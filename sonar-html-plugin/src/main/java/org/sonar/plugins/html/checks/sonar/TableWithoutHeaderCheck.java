7cd2cf55fb58b5c174e5f7f9a50a124c1f16254f
Thomas Serre <118730793+thomas-serre-sonarsource@users.noreply.github.com>
2022-11-30 17:31:21 +0100

    SONARHTML-161 FP S5256: Table content included with Thymeleaf th:block and th:insert (#227)

--- sonar-html-plugin/src/main/java/org/sonar/plugins/html/checks/sonar/TableWithoutHeaderCheck.java
+++ sonar-html-plugin/src/main/java/org/sonar/plugins/html/checks/sonar/TableWithoutHeaderCheck.java
@@ -17,16 +17,21 @@
  */
 package org.sonar.plugins.html.checks.sonar;
 
+import java.util.List;
+import java.util.Set;
 import org.sonar.check.Rule;
 import org.sonar.plugins.html.checks.AbstractPageCheck;
+import org.sonar.plugins.html.node.Attribute;
 import org.sonar.plugins.html.node.TagNode;
 
 @Rule(key = "S5256")
 public class TableWithoutHeaderCheck extends AbstractPageCheck {
 
+  private static final Set<String> THYMELEAF_FRAGMENT_INSERTION_KEYWORDS = Set.of("th:insert", "th:include", "th:replace");
+
   @Override
   public void startElement(TagNode node) {
-    if (isTable(node) && !isLayout(node) && !isHidden(node) && !hasHeader(node)) {
+    if (isTable(node) && !isLayout(node) && !isHidden(node) && !hasHeader(node) && !hasThymeleafFragmentInsertion(node)) {
       createViolation(node, "Add \"<th>\" headers to this \"<table>\".");
     }
   }
@@ -37,12 +42,12 @@ public class TableWithoutHeaderCheck extends AbstractPageCheck {
 
   private static boolean isLayout(TagNode node) {
     String role = node.getAttribute("role");
-    return role != null && ("PRESENTATION".equalsIgnoreCase(role) || "NONE".equalsIgnoreCase(role));
+    return "PRESENTATION".equalsIgnoreCase(role) || "NONE".equalsIgnoreCase(role);
   }
 
   private static boolean isHidden(TagNode node) {
     String ariaHidden = node.getAttribute("aria-hidden");
-    return ariaHidden != null && "TRUE".equalsIgnoreCase(ariaHidden);
+    return "TRUE".equalsIgnoreCase(ariaHidden);
   }
 
   private static boolean hasHeader(TagNode node) {
@@ -52,5 +57,23 @@ public class TableWithoutHeaderCheck extends AbstractPageCheck {
 
   private static boolean isTableHeader(TagNode node) {
     return "TH".equalsIgnoreCase(node.getNodeName());
+  }
+
+  private static boolean hasThymeleafFragmentInsertion(TagNode node) {
+    return hasThymeleafFragmentInsertionFromTableAttribute(node.getAttributes()) || hasThymeleafFragmentInsertionFromTableChildren(node.getChildren());
+  }
+
+  private static boolean hasThymeleafFragmentInsertionFromTableAttribute(List<Attribute> tableAttributes) {
+    return tableAttributes.stream().map(Attribute::getName).anyMatch(attributeName -> attributeName.equals("th:insert") || attributeName.equals("th:include"));
+  }
+
+  private static boolean hasThymeleafFragmentInsertionFromTableChildren(List<TagNode> nodes) {
+    for (TagNode node : nodes) {
+      if (node.getAttributes().stream().map(Attribute::getName).anyMatch(THYMELEAF_FRAGMENT_INSERTION_KEYWORDS::contains)
+        || hasThymeleafFragmentInsertionFromTableChildren(node.getChildren())) {
+        return true;
+      }
+    }
+    return false;
   }
 }
--- sonar-html-plugin/src/test/java/org/sonar/plugins/html/checks/sonar/TableWithoutHeaderCheckTest.java
+++ sonar-html-plugin/src/test/java/org/sonar/plugins/html/checks/sonar/TableWithoutHeaderCheckTest.java
@@ -18,7 +18,6 @@
 package org.sonar.plugins.html.checks.sonar;
 
 import java.io.File;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.sonar.plugins.html.checks.CheckMessagesVerifierRule;
@@ -42,5 +41,6 @@ public class TableWithoutHeaderCheckTest {
         .next().atLine(66)
         .next().atLine(73)
         .next().atLine(78);
+
   }
 }
