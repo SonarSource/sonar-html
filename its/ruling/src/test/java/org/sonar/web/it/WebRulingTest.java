8ffbec6baa849b685a044195635e98ada265e3a5
Yassin Kammoun <52890329+yassin-kammoun-sonarsource@users.noreply.github.com>
2023-06-14 11:26:28 +0200

    Update sonar-analyzer-commons, metadata and RSPEC-s (#242)

--- its/ruling/src/test/java/org/sonar/web/it/WebRulingTest.java
+++ its/ruling/src/test/java/org/sonar/web/it/WebRulingTest.java
@@ -17,18 +17,17 @@
  */
 package org.sonar.web.it;
 
-import com.google.gson.Gson;
 import com.sonar.orchestrator.Orchestrator;
 import com.sonar.orchestrator.build.BuildResult;
 import com.sonar.orchestrator.build.SonarScanner;
-import com.sonar.orchestrator.http.HttpMethod;
+import com.sonar.orchestrator.container.Server;
 import com.sonar.orchestrator.locator.FileLocation;
 import com.sonar.orchestrator.locator.MavenLocation;
 import java.io.File;
 import java.nio.file.Files;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import org.apache.commons.lang.StringUtils;
@@ -36,6 +35,13 @@ import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.sonarsource.analyzer.commons.ProfileGenerator;
+import org.sonarqube.ws.Qualityprofiles;
+import org.sonarqube.ws.client.HttpConnector;
+import org.sonarqube.ws.client.WsClient;
+import org.sonarqube.ws.client.WsClientFactories;
+import org.sonarqube.ws.client.qualityprofiles.ActivateRuleRequest;
+import org.sonarqube.ws.client.qualityprofiles.SearchRequest;
+import org.sonarqube.ws.client.rules.CreateRequest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -43,7 +49,6 @@ public class WebRulingTest {
 
   private static final String LANGUAGE = "web";
   private static final String REPOSITORY_KEY = "Web";
-  private static final Gson GSON = new Gson();
 
   @ClassRule
   public static Orchestrator orchestrator = Orchestrator.builderEnv()
@@ -66,7 +71,7 @@ public class WebRulingTest {
     File litsDifferencesFile = FileLocation.of("target/differences").getFile();
     String projectKey = "project";
     orchestrator.getServer().provisionProject(projectKey, projectKey);
-    orchestrator.getServer().associateProjectToQualityProfile(projectKey, "web", "rules");
+    orchestrator.getServer().associateProjectToQualityProfile(projectKey, LANGUAGE, "rules");
     SonarScanner build = SonarScanner.create()
       .setProjectDir(FileLocation.of("../sources").getFile())
       .setProjectKey(projectKey)
@@ -86,58 +91,78 @@ public class WebRulingTest {
     // To prevent adding error or exception that may be unseen in the logs
     BuildResult result = orchestrator.executeBuild(build);
     List<String> errorList = result.getLogs().lines().filter(line -> line.startsWith("ERROR")).collect(Collectors.toList());
-    assertThat(errorList).hasSize(1);
-    assertThat(errorList.get(0)).contains("decoder-allow-null-chars.html");
+    assertThat(errorList).hasSize(0);
 
     String differences = Files.readString(litsDifferencesFile.toPath());
     assertThat(differences).isEmpty();
   }
 
   private static void instantiateTemplateRule(String ruleTemplateKey, String instantiationKey, String params) {
-    orchestrator.getServer()
-      .newHttpCall("/api/rules/create")
-      .setAdminCredentials()
-      .setMethod(HttpMethod.POST)
-      .setParams(
-        "name", instantiationKey,
-        "markdown_description", instantiationKey,
-        "severity", "INFO",
-        "status", "READY",
-        "template_key", REPOSITORY_KEY + ":" + ruleTemplateKey,
-        "custom_key", instantiationKey,
-        "prevent_reactivation", "true",
-        "params", "name=\"" + instantiationKey + "\";key=\"" + instantiationKey + "\";markdown_description=\"" + instantiationKey + "\";" + params)
-      .execute();
+    newAdminWsClient(orchestrator)
+      .rules()
+      .create(
+        new CreateRequest()
+          .setName(instantiationKey)
+          .setMarkdownDescription(instantiationKey)
+          .setSeverity("INFO")
+          .setStatus("READY")
+          .setTemplateKey(REPOSITORY_KEY + ":" + ruleTemplateKey)
+          .setCustomKey(instantiationKey)
+          .setPreventReactivation("true")
+          .setParams(
+            Arrays.asList(
+              (
+                "name=\"" +
+                instantiationKey +
+                "\";key=\"" +
+                instantiationKey +
+                "\";markdown_description=\"" +
+                instantiationKey +
+                "\";" +
+                params
+              ).split(";", 0)
+            )
+          )
+      );
 
-    // check that the rule has been created
-    String get = orchestrator.getServer()
-      .newHttpCall("api/qualityprofiles/search")
-      .execute()
-      .getBodyAsString();
+    String profileKey = newAdminWsClient(orchestrator)
+      .qualityprofiles()
+      .search(new SearchRequest().setLanguage(LANGUAGE))
+      .getProfilesList()
+      .stream()
+      .filter(qp -> "rules".equals(qp.getName()))
+      .map(Qualityprofiles.SearchWsResponse.QualityProfile::getKey)
+      .findFirst()
+      .orElse(null);
 
-    String profileKey = null;
-    Map map = GSON.fromJson(get, Map.class);
-    for (Map qp : ((List<Map>) map.get("profiles"))) {
-      if ("rules".equals(qp.get("name"))) {
-        profileKey = (String) qp.get("key");
-        break;
-      }
+    if (!StringUtils.isEmpty(profileKey)) {
+      newAdminWsClient(orchestrator)
+        .qualityprofiles()
+        .activateRule(
+          new ActivateRuleRequest()
+            .setKey(profileKey)
+            .setRule(REPOSITORY_KEY + ":" + instantiationKey)
+            .setSeverity("INFO")
+            .setParams(Collections.emptyList())
+        );
+    } else {
+      throw new IllegalStateException(
+        "Could not retrieve profile key : Template rule " +
+        ruleTemplateKey +
+        " has not been activated"
+      );
     }
-    if (StringUtils.isEmpty(profileKey)) {
-      throw new IllegalStateException("Could not retrieve profile key : Template rule " + ruleTemplateKey + " has not been activated");
-    }
-
-    // activate the rule
-    orchestrator.getServer()
-      .newHttpCall("api/qualityprofiles/activate_rule")
-      .setAdminCredentials()
-      .setMethod(HttpMethod.POST)
-      .setParams(
-        "key", profileKey,
-        "rule", REPOSITORY_KEY + ":" + instantiationKey,
-        "severity", "INFO",
-        "params", "")
-      .execute();
   }
 
+  static WsClient newAdminWsClient(Orchestrator orchestrator) {
+    return WsClientFactories
+      .getDefault()
+      .newClient(
+        HttpConnector
+          .newBuilder()
+          .credentials(Server.ADMIN_LOGIN, Server.ADMIN_PASSWORD)
+          .url(orchestrator.getServer().getUrl())
+          .build()
+      );
+  }
 }
