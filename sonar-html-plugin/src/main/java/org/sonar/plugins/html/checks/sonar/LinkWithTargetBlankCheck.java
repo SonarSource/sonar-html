c65bf8308e9054e27028f95f12bc32f54029717f
Yassin Kammoun <52890329+yassin-kammoun-sonarsource@users.noreply.github.com>
2024-11-22 12:17:38 +0100

    SONARHTML-220 Fix FP S5148: Only raise an issue if rel="opener" is explicitly set (#337)

--- sonar-html-plugin/src/main/java/org/sonar/plugins/html/checks/sonar/LinkWithTargetBlankCheck.java
+++ sonar-html-plugin/src/main/java/org/sonar/plugins/html/checks/sonar/LinkWithTargetBlankCheck.java
@@ -18,8 +18,6 @@
 package org.sonar.plugins.html.checks.sonar;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.Locale;
 import java.util.regex.Pattern;
 import org.sonar.check.Rule;
@@ -30,31 +28,35 @@ import org.sonar.plugins.html.node.TagNode;
 public class LinkWithTargetBlankCheck extends AbstractPageCheck {
 
   private static final Pattern DYNAMIC_URL = Pattern.compile("[{}$()\\[\\]]");
-  private static final String NOOPENER = "NOOPENER";
 
   @Override
   public void startElement(TagNode node) {
-    if (isAnchorWithTargetBlank(node) && isInsecureUrl(node) && missingRelAttribute(node)) {
-      createViolation(node, "Make sure not using rel=\"noopener\" is safe here.");
+    if (isAnchor(node) && isInsecureUrl(node) && isVulnerable(node)) {
+      createViolation(node, "Make sure using target=\"_blank\" and rel=\"opener\" is safe here.");
     }
   }
 
-  private static boolean missingRelAttribute(TagNode node) {
-    return !relAttributeValues(node).contains(NOOPENER);
+  private static boolean isVulnerable(TagNode node) {
+    return hasBlankTarget(node) && hasRelOpener(node);
   }
 
-  private static List<String> relAttributeValues(TagNode node) {
+  private static boolean hasBlankTarget(TagNode node) {
+    String target = node.getPropertyValue("TARGET");
+    return "_blank".equalsIgnoreCase(target);
+  }
+
+  private static boolean hasRelOpener(TagNode node) {
     String rel = node.getPropertyValue("REL");
     if (rel == null) {
-      return Collections.emptyList();
+      return false;
     }
     return Arrays.stream(rel.split(" "))
       .map(s -> s.trim().toUpperCase(Locale.ROOT))
-      .toList();
+      .anyMatch("opener"::equalsIgnoreCase);
   }
 
-  private static boolean isAnchorWithTargetBlank(TagNode node) {
-    return node.equalsElementName("A") && "_BLANK".equalsIgnoreCase(node.getPropertyValue("TARGET"));
+  private static boolean isAnchor(TagNode node) {
+    return node.equalsElementName("A");
   }
 
   private static boolean isInsecureUrl(TagNode node) {
