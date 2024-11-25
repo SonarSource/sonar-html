88f531c78dcb531aa5e7494e14d249a3025938a8
Yassin Kammoun <52890329+yassin-kammoun-sonarsource@users.noreply.github.com>
2024-11-25 14:42:57 +0100

    SONARHTML-269 Update license to SONAR Source-Available License v1.0 (SSALv1) (#336)

--- sonar-html-plugin/src/main/java/org/sonar/plugins/html/checks/header/MultiplePageDirectivesCheck.java
+++ sonar-html-plugin/src/main/java/org/sonar/plugins/html/checks/header/MultiplePageDirectivesCheck.java
@@ -1,19 +1,18 @@
 /*
  * SonarSource HTML analyzer :: Sonar Plugin
- * Copyright (c) 2010-2024 SonarSource SA and Matthijs Galesloot
- * sonarqube@googlegroups.com
+ * Copyright (C) 2010-2024 SonarSource SA and Matthijs Galesloot
+ * mailto:info AT sonarsource DOT com
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
  */
 package org.sonar.plugins.html.checks.header;
 
