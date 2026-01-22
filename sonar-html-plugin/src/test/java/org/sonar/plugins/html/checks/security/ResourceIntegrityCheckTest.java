1fd55b71e819c708b1a3b3b84c4a3857a964b638
Loris S. <91723853+loris-s-sonarsource@users.noreply.github.com>
2026-01-22 16:22:58 +0100

    SONARHTML-342 feat: Add support to links to S5725 (#540)

--- sonar-html-plugin/src/test/java/org/sonar/plugins/html/checks/security/ResourceIntegrityCheckTest.java
+++ sonar-html-plugin/src/test/java/org/sonar/plugins/html/checks/security/ResourceIntegrityCheckTest.java
@@ -36,7 +36,17 @@ class ResourceIntegrityCheckTest {
       .next().atLocation(1, 0, 1, 47).withMessage("Make sure not using resource integrity feature is safe here.")
       .next().atLocation(2, 0, 2, 41).withMessage("Make sure not using resource integrity feature is safe here.")
       .next().atLocation(3, 0, 3, 46).withMessage("Make sure not using resource integrity feature is safe here.")
-      .next().atLocation(4, 0, 4, 61).withMessage("Make sure not using resource integrity feature is safe here.");
+      .next().atLocation(4, 0, 4, 61).withMessage("Make sure not using resource integrity feature is safe here.")
+      .next().atLocation(18, 0, 18, 64).withMessage("Make sure not using resource integrity feature is safe here.")
+      .next().atLocation(19, 0, 19, 58).withMessage("Make sure not using resource integrity feature is safe here.")
+      .next().atLocation(20, 0, 20, 63).withMessage("Make sure not using resource integrity feature is safe here.")
+      .next().atLocation(21, 0, 21, 64).withMessage("Make sure not using resource integrity feature is safe here.")
+      .next().atLocation(24, 0, 24, 72).withMessage("Make sure not using resource integrity feature is safe here.")
+      .next().atLocation(25, 0, 25, 67).withMessage("Make sure not using resource integrity feature is safe here.")
+      .next().atLocation(26, 0, 26, 72).withMessage("Make sure not using resource integrity feature is safe here.")
+      .next().atLocation(29, 0, 29, 67).withMessage("Make sure not using resource integrity feature is safe here.")
+      .next().atLocation(30, 0, 30, 61).withMessage("Make sure not using resource integrity feature is safe here.")
+      .next().atLocation(31, 0, 31, 67).withMessage("Make sure not using resource integrity feature is safe here.");
   }
 
 }
