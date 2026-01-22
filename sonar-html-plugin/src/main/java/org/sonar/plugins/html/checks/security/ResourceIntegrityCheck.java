6a3dcca6d8d15be379d0351ef891c889ba65562f
Loris Sierra <loris.sierra@sonarsource.com>
2026-01-22 10:28:50 +0100

    feat: Add support to links to S5725

--- sonar-html-plugin/src/main/java/org/sonar/plugins/html/checks/security/ResourceIntegrityCheck.java
+++ sonar-html-plugin/src/main/java/org/sonar/plugins/html/checks/security/ResourceIntegrityCheck.java
@@ -16,6 +16,7 @@
  */
 package org.sonar.plugins.html.checks.security;
 
+import java.util.Set;
 import org.sonar.check.Rule;
 import org.sonar.plugins.html.checks.AbstractPageCheck;
 import org.sonar.plugins.html.node.Attribute;
@@ -24,14 +25,26 @@ import org.sonar.plugins.html.node.TagNode;
 @Rule(key = "S5725")
 public class ResourceIntegrityCheck extends AbstractPageCheck {
 
+  private static final String MESSAGE = "Make sure not using resource integrity feature is safe here.";
+  private static final Set<String> LINK_REL_VALUES = Set.of("stylesheet", "preload", "modulepreload");
+
   @Override
   public void startElement(TagNode node) {
-    if (node.equalsElementName("script")) {
-      Attribute src = node.getProperty("src");
-      if (src != null && isExternal(src.getValue()) && !node.hasProperty("integrity")) {
-        createViolation(node, "Make sure not using resource integrity feature is safe here.");
-      }
+    if (node.equalsElementName("script") && hasExternalSource(node, "src")) {
+      createViolation(node, MESSAGE);
+    } else if (node.equalsElementName("link") && hasIntegrityRelevantRel(node) && hasExternalSource(node, "href")) {
+      createViolation(node, MESSAGE);
     }
+  }
+
+  private static boolean hasExternalSource(TagNode node, String sourceAttribute) {
+    Attribute source = node.getProperty(sourceAttribute);
+    return source != null && isExternal(source.getValue()) && !node.hasProperty("integrity");
+  }
+
+  private static boolean hasIntegrityRelevantRel(TagNode node) {
+    Attribute rel = node.getProperty("rel");
+    return rel != null && LINK_REL_VALUES.contains(rel.getValue().toLowerCase());
   }
 
   private static boolean isExternal(String srcValue) {
