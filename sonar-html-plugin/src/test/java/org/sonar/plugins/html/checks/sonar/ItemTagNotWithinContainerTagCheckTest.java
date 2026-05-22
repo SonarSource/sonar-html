bbeaaaa508af706f4757365141fb04d540eaf814
Erwan Le Forestier <erwan.leforestier@sonarsource.com>
2026-05-22 15:55:04 +0200

    SONARHTML-159 Reorganize fixtures and revert generated rule HTML

--- sonar-html-plugin/src/test/java/org/sonar/plugins/html/checks/sonar/ItemTagNotWithinContainerTagCheckTest.java
+++ sonar-html-plugin/src/test/java/org/sonar/plugins/html/checks/sonar/ItemTagNotWithinContainerTagCheckTest.java
@@ -31,19 +31,21 @@ class ItemTagNotWithinContainerTagCheckTest {
 
   @Test
   void detected() {
-    HtmlSourceCode sourceCode = TestHelper.scan(new File("src/test/resources/checks/ItemTagNotWithinContainerTagCheck.html"), new ItemTagNotWithinContainerTagCheck());
+    HtmlSourceCode sourceCode = TestHelper.scan(
+        new File("src/test/resources/checks/ItemTagNotWithinContainerTagCheck/invalid-cases.html"),
+        new ItemTagNotWithinContainerTagCheck());
 
     checkMessagesVerifier.verify(sourceCode.getIssues())
         .next().atLocation(1, 0, 1, 4).withMessage("Surround this <li> item tag by a <ul> or <ol> container one.")
         .next().atLocation(4, 0, 4, 4).withMessage("Surround this <DT> item tag by a <dl> container one.")
         .next().atLine(8).withMessage("Surround this <lI> item tag by a <ul> or <ol> container one.")
-        .next().atLine(18);
+        .next().atLine(12);
   }
 
   @Test
-  void templateAncestorsAreCompliant() {
+  void compliant() {
     HtmlSourceCode sourceCode = TestHelper.scan(
-        new File("src/test/resources/checks/ItemTagNotWithinContainerTagCheck/template-aware.html"),
+        new File("src/test/resources/checks/ItemTagNotWithinContainerTagCheck/valid-cases.html"),
         new ItemTagNotWithinContainerTagCheck());
 
     checkMessagesVerifier.verify(sourceCode.getIssues());
