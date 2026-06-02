c8520b7aa17fd24e8f7673f5f8d0a187d51ed9c1
erwan-leforestier-sonarsource <erwan.leforestier@sonarsource.com>
2026-06-02 11:10:10 +0200

    SONARHTML-289 Add rule S8720 for aria-role attribute usage (#683)

---
+++ sonar-html-plugin/src/main/java/org/sonar/plugins/html/checks/accessibility/AriaRoleAttributeCheck.java
@@ -0,0 +1,35 @@
+/*
+ * SonarQube HTML
+ * Copyright (C) SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * You can redistribute and/or modify this program under the terms of
+ * the Sonar Source-Available License Version 1, as published by SonarSource Sàrl.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+package org.sonar.plugins.html.checks.accessibility;
+
+import org.sonar.check.Rule;
+import org.sonar.plugins.html.checks.AbstractPageCheck;
+import org.sonar.plugins.html.node.TagNode;
+
+@Rule(key = "S8720")
+public class AriaRoleAttributeCheck extends AbstractPageCheck {
+
+  private static final String MESSAGE =
+    "Replace \"aria-role\" with \"role\"; \"role\" is the correct WAI-ARIA attribute name.";
+
+  @Override
+  public void startElement(TagNode element) {
+    if (element.hasProperty("aria-role")) {
+      createViolation(element, MESSAGE);
+    }
+  }
+}
