b67f30335c1a9ab617ccc0e2587019fc075a1057
Erwan Le Forestier <erwan.leforestier@sonarsource.com>
2026-09-08 10:54:57 +0200

    SONARHTML-487 Handle conditional descendant IDs

---
+++ sonar-html-plugin/src/test/resources/checks/NoDuplicateIDCheck/conditionalBlocksVueDescendants.vue
@@ -0,0 +1,16 @@
+<template>
+<div v-if="condition">
+  <span id="test">First</span>
+</div>
+<div v-else>
+  <span id="test">Second</span>
+</div>
+<div v-if="showDetails">
+  <span id="inside-branch">First</span>
+  <span id="inside-branch">Second</span>
+</div>
+<span id="shared">Always rendered</span>
+<div v-if="showDetails">
+  <span id="shared">Conditionally rendered</span>
+</div>
+</template>
